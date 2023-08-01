1b898418df2ab82e3f81c5a3434f1225e6faa636
ginodecock <ginodecock@skynet.be>
2023-08-01 08:37:26 +0200

    korter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>from G-DC</h1>
+        <h1>G-DC</h1>
       </header>
     </div>
   );
